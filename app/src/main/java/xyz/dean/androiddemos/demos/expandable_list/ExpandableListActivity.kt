4ac0cb30edec64d801867e1515c46d71625d95f1
deandrezhao <deandrezhao@tencent.com>
2022-03-15 20:29:00 +0800

    [:feat] 可展开列表优化。

--- app/src/main/java/xyz/dean/androiddemos/demos/expandable_list/ExpandableListActivity.kt
+++ app/src/main/java/xyz/dean/androiddemos/demos/expandable_list/ExpandableListActivity.kt
@@ -1,11 +1,15 @@
 package xyz.dean.androiddemos.demos.expandable_list
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import xyz.dean.androiddemos.BaseActivity
 import xyz.dean.androiddemos.DemoItem
 import xyz.dean.androiddemos.R
 import xyz.dean.androiddemos.databinding.ActivityExpandableListBinding
+import xyz.dean.androiddemos.databinding.ItemExpandableListBinding
 
 class ExpandableListActivity : BaseActivity() {
     private lateinit var binding: ActivityExpandableListBinding
@@ -16,13 +20,40 @@ class ExpandableListActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_expandable_list)
         binding.lifecycleOwner = this
-        binding.expandableList.adapter = ExpandableListAdapter()
+        binding.expandableList.adapter = DExpandableListAdapter()
+    }
+
+    class DExpandableListAdapter : ExpandableListAdapter<Int>() {
+        override val expandOneMode: Boolean = true
+        override fun getItemData(position: Int) = position
+
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): ExpandableVH<Int, out ViewDataBinding> {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = ItemExpandableListBinding.inflate(layoutInflater, parent, false)
+            return VH(binding)
+        }
+
+        override fun getItemCount(): Int = 30
+
+        class VH(
+            binding: ItemExpandableListBinding
+        ) : ExpandableVH<Int, ItemExpandableListBinding>(binding) {
+            override val mainItemLayout = binding.itemMainLayout
+            override val expandLayout = binding.itemExpandLayout
+
+            override fun bindData(position: Int, data: Int) {
+                binding.tvName.text = "$position"
+            }
+        }
     }
 
     companion object {
         val demoItem = DemoItem("expandable-list",
             R.string.expandable_list_demo_name,
-            R.string.expandable_list_demo_name,
+            R.string.expandable_list_describe,
             ExpandableListActivity::class.java, R.mipmap.img_practice)
     }
 }
--- app/src/main/java/xyz/dean/androiddemos/demos/expandable_list/ExpandableListAdapter.kt
+++ app/src/main/java/xyz/dean/androiddemos/demos/expandable_list/ExpandableListAdapter.kt
@@ -1,39 +1,96 @@
 package xyz.dean.androiddemos.demos.expandable_list
 
-import android.animation.ObjectAnimator
-import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import xyz.dean.androiddemos.databinding.ItemExpandableListBinding
 
-class ExpandableListAdapter : RecyclerView.Adapter<ExpandableListAdapter.ExpandableVH>() {
+abstract class ExpandableListAdapter<D>
+    : RecyclerView.Adapter<ExpandableListAdapter.ExpandableVH<D, out ViewDataBinding>>()
+{
+    /** 只允许展开一个Item的模式 */
+    open val expandOneMode: Boolean = false
+    private var _expandItem = -1
+    val expandItem: Int get() = _expandItem
+    private val _expandItems = mutableListOf<Int>()
+    val expandItems: List<Int> get() = _expandItems
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExpandableVH {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = ItemExpandableListBinding.inflate(layoutInflater, parent, false)
-        return ExpandableVH(binding)
+    private var recyclerView: RecyclerView? = null
+
+    abstract fun getItemData(position: Int): D
+
+    override fun onBindViewHolder(holder: ExpandableVH<D, out ViewDataBinding>, position: Int) {
+        val isExpand = if (expandOneMode) _expandItem == position else _expandItems.contains(position)
+        holder.bindData(position, isExpand, getItemData(position), this::onExpandStateChanged)
     }
 
-    override fun onBindViewHolder(holder: ExpandableVH, position: Int) {
-        holder.binding.tvName.text = "$position"
-        holder.binding.isExpand = false
-        holder.binding.itemMainLayout.setOnClickListener {
-            holder.binding.apply {
-                if (isExpand != true) {
-                    isExpand = true
-                    itemExpandLayout.animate().scaleY(0f).alpha(0f).start()
-                } else {
-                    isExpand = false
-                    itemExpandLayout.animate().scaleY(1f).alpha(1f).start()
+    private fun onExpandStateChanged(position: Int) {
+        val scrollTo = if (!expandOneMode) {
+            if (_expandItems.contains(position)) {
+                _expandItems.remove(position)
+                notifyItemChanged(position)
+                -1
+            } else {
+                _expandItems.add(position)
+                notifyItemChanged(position)
+                position
+            }
+        } else {
+            val oldExpanded = _expandItem
+            _expandItem = when (_expandItem) {
+                -1 -> position      // 之前没有展开的item，展开此item
+                position -> -1      // 当前item之前是展开的，折叠此item
+                else -> position    // 之前展开的item与当前要展开的item不一样，折叠之前的，展开当前的
+            }
+            if (oldExpanded != -1) notifyItemChanged(oldExpanded)
+            if (_expandItem != -1) notifyItemChanged(_expandItem)
+            _expandItem
+        }
+
+        if (scrollTo != -1) {
+            // 如果要展开的item没有显示完全，则滚动列表使其显示完全。
+            (recyclerView?.layoutManager as? LinearLayoutManager)?.let {
+                val first = it.findFirstCompletelyVisibleItemPosition()
+                val last = it.findLastCompletelyVisibleItemPosition()
+                if (scrollTo > last || scrollTo < first) {
+                    it.isSmoothScrollbarEnabled = true
+                    recyclerView?.smoothScrollToPosition(scrollTo)
                 }
             }
-//            notifyItemChanged(position)
         }
     }
 
-    override fun getItemCount() = 10
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        this.recyclerView = recyclerView
+    }
 
-    class ExpandableVH(
-        val binding: ItemExpandableListBinding
-    ) : RecyclerView.ViewHolder(binding.root)
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView)
+        this.recyclerView = null
+    }
+
+    fun clearExpandState() {
+        _expandItems.clear()
+        _expandItem = -1
+    }
+
+    abstract class ExpandableVH<D, T : ViewDataBinding>(
+        val binding: T
+    ) : RecyclerView.ViewHolder(binding.root) {
+        abstract val mainItemLayout: ViewGroup
+        abstract val expandLayout: ViewGroup
+
+        internal fun bindData(
+            position: Int, isExpand: Boolean, data: D,
+            onExpandChanged: (position: Int) -> Unit,
+        ) {
+            bindData(position, data)
+            expandLayout.isVisible = isExpand
+            mainItemLayout.setOnClickListener { onExpandChanged(position) }
+        }
+
+        abstract fun bindData(position: Int, data: D)
+    }
 }
